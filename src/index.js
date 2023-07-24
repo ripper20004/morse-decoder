d028204f52ce388b9c4837dd986d090df2149eaf
ripper20004 <sashakarosnik@gmail.com>
2023-07-24 15:15:08 +0400

    fix: refactoring code

--- src/index.js
+++ src/index.js
@@ -39,20 +39,24 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     let arrMorse = [];
-    for (let i = 0; i < expr.length; i+=10) {
+
+    for (let i = 0; i < expr.length; i += 10) {
         let strMorse = '';
-        for (let j = 0; j < 10; j+=2) {
-            if (expr[j+i] === "1" && expr[i+j+1] === "0") {
+
+        for (let j = 0; j < 10; j += 2) {
+            if (expr[j + i] === "1" && expr[i + j + 1] === "0") {
                 strMorse += '.'
-            } else if (expr[j+i] === "1" && expr[j+i+1] === "1") {
+            } else if (expr[j + i] === "1" && expr[j + i + 1] === "1") {
                 strMorse += '-'
-            } else if (expr[j+i] === "*" && expr[j+i+1] === "*") {
+            } else if (expr[j + i] === "*" && expr[j + i + 1] === "*") {
                 strMorse += ' '
                 break;
             }
         }
+
         arrMorse.push(strMorse);
     }
+
     return arrMorse.map((item) => {
         if (item === " ") return " ";
         return MORSE_TABLE[item];
